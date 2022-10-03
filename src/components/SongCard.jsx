f311153129951df3757d550111c60bc8a6f40e75
fil <filipisee@hotmail.com>
2022-10-03 18:16:57 -0300

    finished play/pause component

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -5,9 +5,16 @@ import PlayPause from "./PlayPause";
 import { playPause, setActiveSong } from "../redux/features/playerSlice";
 
 const SongCard = ({ song, i, activeSong, isPlaying, data }) => {
-  const handlePauseClick = () => {};
+  const dispatch = useDispatch();
 
-  const handlePlayClick = () => {};
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+
+  const handlePlayClick = () => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  };
 
   return (
     <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
