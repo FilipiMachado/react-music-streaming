982b4086c5d69b727a5431230e0e40b056a2ef69
fil <filipisee@hotmail.com>
2022-10-03 22:36:48 -0300

    working on song details page

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,3 +1,20 @@
-const SongDetails = () => <div>SongDetails</div>;
+import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+
+import { DetailsHeader, Error, Loader, RelatedSongs } from "../components";
+
+import { setActiveSong, playPause } from "../redux/features/playerSlice";
+
+const SongDetails = () => {
+  const { songid } = useParams();
+
+  console.log(songid);
+
+  return (
+    <div>
+      <div>1</div>
+    </div>
+  );
+};
 
 export default SongDetails;
