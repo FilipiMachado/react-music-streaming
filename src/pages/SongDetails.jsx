edadf3b6ba190dba2c30a923cedb4b05addbc44d
fil <filipisee@hotmail.com>
2022-10-05 14:43:57 -0300

    styling song details headers

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -17,7 +17,7 @@ const SongDetails = () => {
 
   return (
     <div className="flex flex-col">
-      {/* <DetailsHeader artistId={artistId} songData={songData} /> */}
+      <DetailsHeader artistId="" songData={songData} />
 
       <div className="mb-10">
         <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
@@ -25,9 +25,9 @@ const SongDetails = () => {
 
       <div className="mt-5">
         {songData?.sections[1].type === "LYRICS" ? (
-          songData?.sections[1].text.map((line, i) => <p>{line}</p>)
+          songData?.sections[1].text.map((line, i) => <p className="text-gray-400 text-base my-1">{line}</p>)
         ) : (
-          <p>Sorry no lyrics found!</p>
+          <p className="text-gray-400 text-base my-1">Sorry, no lyrics found!</p>
         )}
       </div>
     </div>
