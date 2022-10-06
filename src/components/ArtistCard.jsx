69c4574d02986c88718609d54ffb2eab7041e078
fil <filipisee@hotmail.com>
2022-10-06 14:44:47 -0300

    styling artist card comp

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -4,12 +4,18 @@ const ArtistCard = ({ track }) => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backgrop-blur-sm animate-slideup rounded-lg cursor-pointer">
+    <div
+      onClick={() => navigate(`/artists/${track?.artists[0].adamid}`)}
+      className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backgrop-blur-sm animate-slideup rounded-lg cursor-pointer"
+    >
       <img
         className="w-full h-56 rounded-lg"
         src={track?.images?.coverart}
         alt="artist"
       />
+      <p className="mt-4 font-semibold text-lg text-white truncate">
+        {track?.subtitle}
+      </p>
     </div>
   );
 };
