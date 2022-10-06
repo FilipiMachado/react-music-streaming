323d191612f20efc6099c128e0764cd247507072
fil <filipisee@hotmail.com>
2022-10-06 14:36:33 -0300

    structuring top artist page

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -1,5 +1,17 @@
-const ArtistCard = () => {
-  <div>ArtistCard</div>;
+import { useNavigate } from "react-router-dom";
+
+const ArtistCard = ({ track }) => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backgrop-blur-sm animate-slideup rounded-lg cursor-pointer">
+      <img
+        className="w-full h-56 rounded-lg"
+        src={track?.images?.coverart}
+        alt="artist"
+      />
+    </div>
+  );
 };
 
 export default ArtistCard;
